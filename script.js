7422cb75ac1ffe91dc7b72e789b216237cf65dc5
asif <asif@gmail.com>
2022-02-12 19:24:02 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function whatIsNetflix(block,block2,block3,block4,block5,block6,none,none2,none3,none4,none5,none6){
+    document.getElementById(block).style.display="block";
+    document.getElementById(block2).style.display="block";
+    document.getElementById(block3).style.display="block";
+    document.getElementById(block4).style.display="block";
+    document.getElementById(block5).style.display="block";
+    document.getElementById(block6).style.display="block";
+    document.getElementById(none).style.display="none";
+    document.getElementById(none2).style.display="none";
+    document.getElementById(none3).style.display="none";
+    document.getElementById(none4).style.display="none";
+    document.getElementById(none5).style.display="none";
+    document.getElementById(none6).style.display="none";
+   }
